c8aa3686fb5c0da3019e780f66495c87580385a5
Filipe Ferreira <fferreira@softjournua.onmicrosoft.com>
2023-09-25 12:51:10 -0300

    Add entity model

---
+++ Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace FFSIT.Model.Entities
+{
+    public class BaseEntity
+    {
+        public long Id { get; private set; }
+
+        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; private set; }
+
+        public DateTime? DeletedAt { get; private set; }
+
+        public bool IsDeleted => DeletedAt.HasValue;
+
+        public void Delete()
+        {
+            DeletedAt = DateTime.Now;
+        }
+    }
+}
